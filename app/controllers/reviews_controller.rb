8642e47459f28f4a94e6b892bbe5e207bb5ec6a3
Richard Haenel <richardhaenel1@gmail.com>
2019-06-05 16:55:07 +0200

    d

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,30 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
   def new
     # we need @restaurant in our `simple_form_for`
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @review = Review.new(review_params)
+    @review = @restaurant.reviews.new(review_params)
     if @review.save
-      redirect_to restaurant_id_path(@restaurant_id)
+      redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'new'
     end
     # we need `restaurant_id` to asssociate review with corresponding restaurant
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    # @review.restaurant = Restaurant.find(params[:restaurant_id])
+    # @review.save
   end
 
   private
 
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
